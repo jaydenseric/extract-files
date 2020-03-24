80a23a8bbe998d0621d30ec5408bd489e38c9ee9
Jayden Seric <me@jaydenseric.com>
2020-03-24 21:34:06 +1100

    Update the test-director dev dependency.

--- src/test/index.mjs
+++ src/test/index.mjs
@@ -1,9 +1,9 @@
-import testDirector from 'test-director'
+import { TestDirector } from 'test-director'
 import ReactNativeFile from './lib/ReactNativeFile.test.mjs'
 import extractFiles from './lib/extractFiles.test.mjs'
 import isExtractableFile from './lib/isExtractableFile.test.mjs'
 
-const tests = new testDirector.TestDirector()
+const tests = new TestDirector()
 
 ReactNativeFile(tests)
 extractFiles(tests)
